c7ef324e353aece032195bba8f5df21566e89bd5
CDelacruz1802 <carlos.de.la.cruz@accenture.com>
2024-07-24 15:40:41 -0700

    Creacion del componente InfoCard - Implementacion del get para obtener los elementos de la api -  creacion del bot de voz

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import "./App.css";
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+import InfoCard from "./components/InfoCardComponent/InfoCard";
+
 function App() {
-  return <>Hola mundo</>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Funcion para obtener los datos
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://rickandmortyapi.com/api/character"
+        );
+        setData(JSON.stringify(response.data.results, null, 2));
+        // console.log(response.data.results);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <InfoCard data={data} texto={"Hola"} />;
 }
 
 export default App;
