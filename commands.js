55dec58a18b4fecebe80b9700f10cff0ff4ccd7c
CDelacruz1802 <carlos.de.la.cruz@accenture.com>
2024-07-24 13:10:23 -0700

    Primer commit

---
+++ commands.js
@@ -0,0 +1,6 @@
+//* Como conectarse a un proyecto con git ya inicializado.
+//? git remote add origin https://github.com/Delacruz188/tasks-app.git
+//? git branch -M main
+//? git push -u origin main
+
+//? npm i react-beautiful-dnd
