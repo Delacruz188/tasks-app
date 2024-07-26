8e57e6e856a2fc4136ca5f429f3bcbf4e9980e95
Carlos De La Cruz <delacruzc410@gmail.com>
2024-07-26 15:59:04 -0600

    Avances primera sesion de react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+import ListComponent from "./components/ListComponent/ListComponent";
 
 function App() {
-  return <>Hola mundo</>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://rickandmortyapi.com/api/character"
+        );
+        // setData(JSON.stringify(response.data.results, null, 2));
+        console.log(response.data.results);
+      } catch (err) {
+        // setError(err.message);
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <ListComponent nombre={"informacion desde el padre"} />;
 }
 
 export default App;
---
+++ src/components/ListComponent/ListComponent.jsx
@@ -0,0 +1,7 @@
+export default function ListComponent({ nombre }) {
+  return (
+    <div>
+      <h1>{nombre}</h1>
+    </div>
+  );
+}
